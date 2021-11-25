55d100879f247bd83fce85f555c07f0ab66e86a3
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-11-24 23:35:10 -0600

    Nombres de variables

---
+++ src/NamingJava.java
@@ -0,0 +1,18 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 3337777;
+        int celPhone = 5557777;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Guatemala";
+        String _backgroundColor = "Green";
+
+        String currency$ = "GT";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+    }
+}
