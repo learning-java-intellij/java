a672cf78ee866922308538e28ae7895e76a5980b
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-11-25 23:48:25 -0600

    Sentencia switch

---
+++ src/SwitchStatement.java
@@ -0,0 +1,22 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+        String colorModeSelected = "Dark";
+
+        switch (colorModeSelected) {
+            case "Light":
+                System.out.println("Seleccionaste Light Mode");
+                break;
+            case "Night": //Ambar
+                System.out.println("Seleccionaste Ambar Mode");
+                break;
+            case "Blue Dark":
+                System.out.println("Seleccionaste Blue Dark Mode");
+                break;
+            case "Dark":
+                System.out.println("Seleccionaste Dark Mode");
+                break;
+            default:
+                System.out.println("Selecciona una opión");
+        }
+    }
+}
