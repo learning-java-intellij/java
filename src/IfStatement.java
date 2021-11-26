310c1881877f4510111733b1e62e57a4fff5cd2e
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-11-25 23:35:50 -0600

    Sentencia if-else

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,15 +1,17 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int fileSended = 3;
 
         if (isBluetoothEnabled) {
             //Send file
             fileSended++;
             System.out.println("Archivo enviado");
+        } else {
+            System.out.println("Por favor enciende tu Bluetooth, para iniciar la transferencia");
         }
 
-        System.out.println(|isBluetoothEnabled);
+        System.out.println(isBluetoothEnabled);
         System.out.println(fileSended);
     }
 }
