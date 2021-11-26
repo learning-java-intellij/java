8e1e6312690a2be7ecbf8e35047a101c31191161
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-11-25 23:18:32 -0600

    Casteos

---
+++ src/Casting.java
@@ -0,0 +1,18 @@
+public class Casting {
+    public static void main(String[] args) {
+        //En un año ubicar 30 perritos
+        //Cuántos perritos ubiqué al mes
+
+        double monthlyDogs = 30.0/12.0;
+        System.out.println(monthlyDogs);
+
+        //Estimación
+        int estimatedMontlyDogs = (int) monthlyDogs;
+        System.out.println(estimatedMontlyDogs);
+
+        //Exactitud
+        int a = 30;
+        int b = 12;
+        System.out.println((double)a/b);
+    }
+}
