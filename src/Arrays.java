4b00dedb2bdd2ec340b1964290da0cd8551d7192
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-11-27 21:32:10 -0600

    Declaración de arreglos

---
+++ src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2]; //4*2 = 8
+        int[][][] numbers = new int[2][2][2];
+        int[][][][] numbers4 = new int[2][2][2][2];
+    }
+}
