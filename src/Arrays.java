62d5af7582db5eadcc1af74f729b5da755b6742a
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-11-27 21:50:56 -0600

    Índices

--- src/Arrays.java
+++ src/Arrays.java
@@ -14,10 +14,14 @@ public class Arrays {
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
 
-        System.out.println(androidVersions[0]);
+        for (int i = 0; i < androidVersions.length ; i++) {
+            System.out.println(androidVersions[i]);
+        }
+
+        /*System.out.println(androidVersions[0]);
         System.out.println(androidVersions[1]);
         System.out.println(androidVersions[2]);
-        System.out.println(androidVersions[3]);
+        System.out.println(androidVersions[3]);*/
 
         cities[0][0] = "Colombia";
         cities[0][1] = "Medillín";
@@ -28,18 +32,34 @@ public class Arrays {
         cities[3][0] = "México";
         cities[3][1] = "CDMX";
 
-        System.out.println(cities[0][0]);
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+
+        /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
 
         String[][][][] animals = new String[2][3][2][2];
         animals[1][0][0][1] = "Monkey";
 
-        System.out.println(animals[1][0][0][1]);
+        for (int i = 0; i < animals.length; i++) {
+            for (int j = 0; j < animals[i].length; j++) {
+                for (int k = 0; k < animals[i][j].length; k++) {
+                    for (int l = 0; l < animals[i][j][k].length; l++) {
+                        System.out.println(animals[i][j][k][l]);
+                    }
+                }
+            }
+        }
+
+        /*System.out.println(animals[1][0][0][1]);*/
     }
 }
