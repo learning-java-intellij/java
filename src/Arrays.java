156aa6b3b4779f23c5c722e00378f88befbe5f4c
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-11-27 21:41:18 -0600

    Asignación de arreglos

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Arrays {
     public static void main(String[] args) {
         String[] androidVersions = new String[17];
@@ -6,5 +8,38 @@ public class Arrays {
         String[][] cities = new String[4][2]; //4*2 = 8
         int[][][] numbers = new int[2][2][2];
         int[][][][] numbers4 = new int[2][2][2][2];
+
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);
+
+        cities[0][0] = "Colombia";
+        cities[0][1] = "Medillín";
+        cities[1][0] = "Colombia";
+        cities[1][1] = "Bogotá";
+        cities[2][0] = "México";
+        cities[2][1] = "Guadalajara";
+        cities[3][0] = "México";
+        cities[3][1] = "CDMX";
+
+        System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);
+
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1] = "Monkey";
+
+        System.out.println(animals[1][0][0][1]);
     }
 }
